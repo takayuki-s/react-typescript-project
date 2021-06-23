b9b1052a2014f7d64f696c69ddf893f10bdf85d2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-24 08:40:03 +0900

    propsの型指定（完了）

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,11 @@
 type TodoType = {
   userId: number;
   title: string;
-  completed: boolean;
+  completed?: boolean;
 };
 
 export const Todo = (props: TodoType) => {
-  const { title, userId, completed } = props;
+  const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark}${title}(ユーザー：${userId})`}</p>;
 };
